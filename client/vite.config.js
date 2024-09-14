4efbd0ad8ccd7646f0425caa7a09c6b1f6a380c2
tprasant007 <123845146+tprasant007@users.noreply.github.com>
2024-09-15 00:55:44 +0100

    Update backend url in vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:8000",
+        target: "https://workout-buddy-backend-6l0x.onrender.com/",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, "/api"),
       },
