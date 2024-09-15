00c1e95940aa5794efd739f02d3064159571a91b
tprasant007 <123845146+tprasant007@users.noreply.github.com>
2024-09-15 14:44:15 +0100

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,27 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 3000,
-    proxy: {
-      "/api": {
-        target: "https://workout-buddy-backend-6l0x.onrender.com/",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, "/api"),
-      },
-    },
-  },
   plugins: [react()],
-},
-{
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://workout-buddy-backend-6l0x.onrender.com/',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
 })
